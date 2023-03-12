526c3434ba5ee4177b39c7b2764b2ecd38f7e658
Jerome Lloyd Garcia <113158800+477AFD@users.noreply.github.com>
2023-03-12 15:10:42 +0800

    Rewrite firmware code for motor board
    The code is replaced with a more simpler code. It uses both analog and digital I/O to provide better flexibility.

--- INSTALL_MOTOR.ino
+++ INSTALL_MOTOR.ino
@@ -1,204 +1,81 @@
-// Version a1.0
-#define dat1 A0
-#define dat2 A1
-#define dat3 A2
-#define dat4 A3
-#define dat5 A4
-#define dat6 A5
-
 #include <AFMotor.h>
 #include <Servo.h>
 
+// Define Servos
 Servo directional;
 Servo turner;
 
-AF_DCMotor motorLB(1, MOTOR12_1KHZ);
-AF_DCMotor motorRB(2, MOTOR12_1KHZ);
-AF_DCMotor motorLT(3, MOTOR12_1KHZ);
-AF_DCMotor motorRT(4, MOTOR12_1KHZ);
+int bit_analog(uint8_t AX) {
+  return map(analogRead(AX), 0, 1023, 0, 255);
+}
+AF_DCMotor motorA(1, MOTOR12_64KHZ);
+AF_DCMotor motorB(2, MOTOR12_64KHZ);
+
+void moveUnit(int speed, int d1, int d2) {
+motorA.setSpeed(speed);
+motorB.setSpeed(speed);
+int direction = (d1 * 2) + (d2 * 1);
+if (direction == 3 | direction == 0) {
+  motorA.run(RELEASE);
+} else {
+  switch (direction) {
+    case 1:
+      motorA.run(FORWARD);
+      break;
+    case 2:
+      motorA.run(BACKWARD);
+      break;
+  }
+}
+}
 
 void setup() {
-pinMode(dat1, INPUT); //Data (directional sensor)
-pinMode(dat2, INPUT); //Data (special)
-pinMode(dat3, INPUT); //Data (Bit 0)
-pinMode(dat4, INPUT); //Data (Bit 1)
-pinMode(dat5, INPUT); //Data (Bit 2)
-pinMode(dat6, INPUT); //Data (Bit 3)
-pinMode(0, OUTPUT); // RX pin (12V)
-pinMode(1, OUTPUT); // TX pin (VACUUM)
-pinMode(2, INPUT); // RESPONSE
-pinMode(13, OUTPUT); // COUNTER-RESPONSE
+pinMode(13, OUTPUT);
+  /* those are not used because of static
+pinMode(12, INPUT);
+pinMode(A4, INPUT);
+pinMode(A3, INPUT);
+pinMode(A1, INPUT);
+pinMode(A0, INPUT); */
+pinMode(0, OUTPUT);
+pinMode(1, OUTPUT);
+// Calibrate the sensor's servo
 directional.attach(9);
 turner.attach(10);
-digitalWrite(0, HIGH);
-delay(3000);
-digitalWrite(1, HIGH);
-while (digitalRead(2) == LOW) {
-  delayMicroseconds(10);
-}
-digitalWrite(13, HIGH);
-delay(500);
-digitalWrite(13, LOW);
+directional.write(0);
+delay(1000);
+directional.write(180);
+delay(1000);
+directional.write(90);
+turner.write(66);
+digitalWrite(0, LOW);
+delay(1000);
+turner.write(113);
+delay(1000);
+turner.write(90);
+digitalWrite(1, LOW);
+
+// Signal the main Arduino, telling that it is ready
+digitalWrite(13, digitalRead(0));
 }
 
-void directionalMove(int degrees) {
-  int servo_RawDegrees = map(degrees, -90, 90, 0, 180);
-  directional.write(servo_RawDegrees);  
-}
-
-void turnGear(int direction) {
-  switch (direction) {
-    case -1:
-      turner.write(0);
-      break;
-    case 0:
-      turner.write(90);
-      break;
+void turnUnit(int t1, int t2) {
+  int data = (t2 * 1) + (t1 * 2);
+  switch (data) {
     case 1:
-      turner.write(180);
+      turner.write(66);
       break;
-  }
-}
-
-void moveUnit(int directionMT, int speed) {
-int speedrw;
-  if (speed <= 0) { // it can cause an issue when speed is 0, because X / 0 and negative is not defined/tolerated
-    speedrw = 255;
-  } else {
-    speedrw = 255 / (speed); // the higher the value, the lower the speed
-  }
-  motorLB.setSpeed(speedrw);
-  motorLT.setSpeed(speedrw);
-  motorRB.setSpeed(speedrw);
-  motorRT.setSpeed(speedrw);
-  switch (directionMT) {
-    case -1:
-      motorLB.run(BACKWARD);
-      motorLT.run(BACKWARD);
-      motorLB.run(BACKWARD);
-      motorLB.run(BACKWARD);
-      break;
-    case 0:
-      motorLB.run(RELEASE);
-      motorLT.run(RELEASE);
-      motorLB.run(RELEASE);
-      motorLB.run(RELEASE);
-      break;
-    case 1:
-      motorLB.run(FORWARD);
-      motorLT.run(FORWARD);
-      motorLB.run(FORWARD);
-      motorLB.run(FORWARD);
-      break;         
-  }
-}
-
-void sendCT() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-}
-
-int receiveData() {
-  // Data 0-5 (000000), wherein last bit is data 0, first bit is data 5 (6-bit value)
-  int data0 = digitalRead(dat1);
-  int data1 = digitalRead(dat2);
-  int data2 = digitalRead(dat3);
-  int data3 = digitalRead(dat4);
-  int data4 = digitalRead(dat5);
-  int data5 = digitalRead(dat6);
-  return ((data0 * 1) + (data1 * 2) + (data2 * 4) + (data3 * 8) + (data4 * 16) + (data5 * 32)); // this can output from 0 to 64
-}
-
-int resultData;
-int frame = -1;
-
-
-void loop() {
-  frame = frame + 1;
-  while (digitalRead(2) == LOW) {
-    resultData = receiveData();
-    delay(500);
-  } 
-  switch (resultData) {
-    case 0:
-      moveUnit(0, 1);
-      sendCT();
-      break;
-    case 4:
-      moveUnit(1, 4);
-      sendCT();
-      break;
-    case 5:
-      moveUnit(1, 3);
-      sendCT();
-      break;
-    case 6:
-      moveUnit(1, 2);
-      sendCT();
-      break;
-    case 7:
-      moveUnit(1, 1);
-      sendCT();
-      break;
-    case 8:
-      moveUnit(-1, 4);
-      sendCT();
-      break;
-    case 9:
-      moveUnit(-1, 3);
-      sendCT();
-      break;
-    case 10:
-      moveUnit(-1, 2);
-      sendCT();
-      break;    
-    case 11:
-      moveUnit(-1, 1);
-      sendCT();
-      break;
-    case 16:
-      turnGear(-1);
-      sendCT();
-      break;
-    case 20:
-      turnGear(1);
-      sendCT();
-      break;
-    case 28:
-      turnGear(0);
-      sendCT();
-      break;
-    case 32:
-      directionalMove(-90);
-      sendCT();
-      break;
-    case 33:
-      directionalMove(0);
-      sendCT();
-      break;
-    case 34:
-      sendCT();
-      break;
-    case 35:
-      sendCT();
-      break;
-    case 36:
-      directionalMove(90);
-      sendCT();
-      break;
-    case 37:
-      sendCT();
-      break;
-    case 38:
-      sendCT();
-      break;
-    case 39:
-      sendCT();
+    case 2:
+      turner.write(113);
       break;
     default:
-      moveUnit(0, 1);
-      sendCT();
-      break;
+      turner.write(90);
+      break;  
   }
+}
+
+void loop() {
+  moveUnit(analogRead(A5), digitalRead(A4), digitalRead(A3));
+  directional.write(bit_analog(A2));
+  turnUnit(digitalRead(A1), digitalRead(A0));
 }
